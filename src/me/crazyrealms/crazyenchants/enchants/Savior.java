3a70dc1ad38d791b3f09821969efe4ce594c4f55
Danny A <ipodtouch0218@gmail.com>
2017-03-20 15:16:30 -0400

    Apparently i have to do it to master

--- src/me/crazyrealms/crazyenchants/enchants/Savior.java
+++ src/me/crazyrealms/crazyenchants/enchants/Savior.java
@@ -1,5 +1,11 @@
 package me.crazyrealms.crazyenchants.enchants;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
 import me.crazyrealms.crazyenchants.Enchant;
 import me.crazyrealms.crazyenchants.enums.ItemSet;
 import me.crazyrealms.crazyenchants.enums.Rarity;
@@ -10,4 +16,18 @@ public class Savior extends Enchant {
 		super("Savior", 4, Rarity.LEGENDARY, new ItemSet[]{ItemSet.HELMET}, "Chance to heal when low on health", 20, false);
 	}
 	
+	@Override
+	public void genericDamageEvent(EntityDamageEvent e) {
+		if (e instanceof Player) return; //Not a player, can't have savior.
+		Player pl = (Player) e.getEntity();
+		
+		if (pl.getHealth()/pl.getMaxHealth() <= .20) { //Less than 20% health remaining, <= 2 full hearts.
+			pl.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 2, 5));
+		}
+	}
+	
+	@Override
+	public void playerHitEvent(EntityDamageByEntityEvent e) {
+		genericDamageEvent(e);
+	}
 }
