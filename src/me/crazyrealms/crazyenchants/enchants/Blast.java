ecef03f4bcde796a0cf8b2e5ad781afe518a333a
Danny A <ipodtouch0218@gmail.com>
2017-03-20 14:13:23 -0400

    Change.

--- src/me/crazyrealms/crazyenchants/enchants/Blast.java
+++ src/me/crazyrealms/crazyenchants/enchants/Blast.java
@@ -1,13 +1,35 @@
 package me.crazyrealms.crazyenchants.enchants;
 
+import org.bukkit.block.Block;
+import org.bukkit.event.block.BlockBreakEvent;
+
 import me.crazyrealms.crazyenchants.Enchant;
 import me.crazyrealms.crazyenchants.enums.ItemSet;
 import me.crazyrealms.crazyenchants.enums.Rarity;
 
 public class Blast extends Enchant {
 
+	//Create Enchantment
 	public Blast() {
 		super("Blast", 3, Rarity.EPIC, new ItemSet[]{ItemSet.PICKAXE, ItemSet.SHOVEL}, "When mining, you will destroy in a radius of blocks increasing by each level", 100, false);
 	}
 	
+	@Override
+	public void playerBreakBlockEvent(BlockBreakEvent e) {
+		if (e.getPlayer().getItemInHand() == null) return; //Somehow this was called, even if player has no item? Whatever.
+		int level = Enchant.getEnchants(e.getPlayer().getItemInHand()).get(this); //Loop through item's custom enchants, return level.
+		int size = 1 + (level * 2); //Size of the blast. Lv 1= 3x3, Lv2 = 5x5, Lv3 = 7x7 ***CHANGE IF NECESSARY***
+		
+		for (int x = -size; x < size; x++) { 		  //Loop through X coordinates
+			for (int y = -size; y < size; y++) {	  //Loop through Y coordinates
+				for (int z = -size; z < size; z++) {  //Loop through Z coordinates
+					
+					Block block = e.getBlock().getRelative(x, y, z); //Block to be removed, relative to e.getBlock()
+					
+					block.breakNaturally(e.getPlayer().getItemInHand()); //Break block & Drop item, if item used is correct.
+					
+				}
+			}
+		}
+	}
 }
--- src/me/crazyrealms/crazyenchants/enchants/Haste.java
+++ src/me/crazyrealms/crazyenchants/enchants/Haste.java
@@ -1,5 +1,21 @@
 package me.crazyrealms.crazyenchants.enchants;
 
-public class Haste {
+import org.bukkit.entity.Player;
 
+import me.crazyrealms.crazyenchants.Enchant;
+import me.crazyrealms.crazyenchants.enums.ItemSet;
+import me.crazyrealms.crazyenchants.enums.Rarity;
+
+public class Haste extends Enchant {
+
+	public Haste() {
+		super("Haste", 3, Rarity.SIMPLE, new ItemSet[]{ItemSet.ALL_TOOLS}, "Gives the haste effect to the player", 100, true);
+	}
+	
+	@Override
+	public void alwaysActive(Player player) {
+		// TODO Auto-generated method stub
+		super.alwaysActive(player);
+	}
+	
 }
