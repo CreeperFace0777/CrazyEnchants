eefceff2f1ccf5ccc590445d88dcfb65d8b7d91b
Jack Chapman <jack.chapman77@hotmail.co.uk>
2017-03-21 19:27:41 +0000

    Added XP.java

---
+++ src/me/crazyrealms/crazyenchants/enchants/XP.java
@@ -0,0 +1,19 @@
+package me.crazyrealms.crazyenchants.enchants;
+
+import me.crazyrealms.crazyenchants.Enchant;
+import me.crazyrealms.crazyenchants.enums.ItemSet;
+import me.crazyrealms.crazyenchants.enums.Rarity;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+public class XP extends Enchant {
+    XP() {
+        super("XP", 4, Rarity.LEGENDARY, new ItemSet[]{ItemSet.SWORD}, "Gives the player more experience points when killing a mob", 45, false);
+    }
+
+    @Override
+    public void entityDeathEvent(EntityDeathEvent e) {
+        //TODO: GET MORE XP
+        
+    }
+
+}
