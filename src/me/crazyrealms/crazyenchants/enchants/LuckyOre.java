8fa0c2393c50e9e2613adf22942e278341f43d22
Danny A <ipodtouch0218@gmail.com>
2017-03-22 19:50:59 -0400

    actually finished the drops :D

--- src/me/crazyrealms/crazyenchants/enchants/LuckyOre.java
+++ src/me/crazyrealms/crazyenchants/enchants/LuckyOre.java
@@ -3,6 +3,7 @@ package me.crazyrealms.crazyenchants.enchants;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.bukkit.Material;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -20,12 +21,28 @@ public class LuckyOre extends Enchant {
 	public void playerBreakBlockEvent(BlockBreakEvent e) {
 		Collection<ItemStack> newDrops = new ArrayList<ItemStack>();		
 		
-		if (e.getPlayer().getItemInHand() == null) {
-		newDrops = e.getBlock().getDrops(e.getPlayer().getItemInHand());
-		for (ItemStack drops : e.getBlock().getDrops()) {
+		newDrops = e.getBlock().getDrops(e.getPlayer().getItemInHand()); //Don't need a null check.
+		for (ItemStack drops : newDrops) {
 			if (drops.getType().name().contains("_ORE")) {
-				
+				ItemStack newStack = getSmeltedForm(drops.getType());
+				newDrops.remove(drops);
+				newDrops.add(newStack);
 			}
+		}
+	}
+	
+	private ItemStack getSmeltedForm(Material m) {
+		switch (m) {
+			case COAL_ORE: return new ItemStack(Material.COAL);
+			case IRON_ORE: return new ItemStack(Material.IRON_INGOT);
+			case GOLD_ORE: return new ItemStack(Material.GOLD_INGOT);
+			case QUARTZ_ORE: return new ItemStack(Material.QUARTZ);
+			case DIAMOND_ORE: return new ItemStack(Material.DIAMOND);
+			case EMERALD_ORE: return new ItemStack(Material.EMERALD);
+			case LAPIS_ORE: return new ItemStack(Material.INK_SACK, 1, (short) 4);
+			case REDSTONE_ORE: return new ItemStack(Material.REDSTONE);
+			case GLOWING_REDSTONE_ORE: return new ItemStack(Material.REDSTONE);
+		default: return null;
 		}
 	}
 }
